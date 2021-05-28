913adba3a23cec4b3fa1cebdb33ea4b270f23cd4
Rameshwar-Ishnava <84984682+Rameshwar-Ishnava@users.noreply.github.com>
2021-05-29 00:27:25 +0530

    Update Demo.php

--- Day 2/Flow Control statement/Demo.php
+++ Day 2/Flow Control statement/Demo.php
@@ -1,3 +1,21 @@
 <?php
- echo"Hello";
+$name = "Rameshwar";
+$age = 23;
+ 
+if ($age < 20)
+{
+  echo "$name Your age is less than 20!";
+}
+elseif ($age > 20 && $age < 30)
+{
+  echo "$name Your age is between 20 and 30!";
+}
+elseif ($age > 30 && $age < 40)
+{
+  echo "$name Your age is between 30 and 40!";
+}
+else
+{
+  echo "$name Your age is greater than 40!";
+}
 ?>
