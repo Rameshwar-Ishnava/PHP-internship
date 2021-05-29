7a352d05bc9f2d82dd7663d49061c4a386528758
Rameshwar-Ishnava <84984682+Rameshwar-Ishnava@users.noreply.github.com>
2021-05-30 00:28:45 +0530

    Add files via upload

---
+++ Day 3/output_display.php
@@ -0,0 +1,24 @@
+<?php
+$fullname = $_POST['fname'];
+$email = $_POST['email'];
+$mobile = $_POST['mobile'];
+$dob = $_POST['dob'];
+$gender = $_POST['gender'];
+$hobbies = $_POST['hobbies'];
+$city = $_POST['city'];
+$address = $_POST['address'];
+$username = $_POST['username'];
+$password = $_POST['pass'];
+
+
+echo "	Full Name    	= $fullname <br/>
+		Email 			= $email <br/>
+		Mobile Number 	= $mobile <br/> 
+		D.O.B. 			= $dob <br/>
+		Gender 			= $gender <br/> 
+		Hobbies			= $hobbies <br/>
+		City 			= $city <br/>
+		Address 		= $address <br/>
+		Username     	= $username <br/>
+		Password 		= $password ";
+?>
