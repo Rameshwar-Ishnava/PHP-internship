e36e23ca7cb0ae22251e8b2d45de10388cf4e18f
Rameshwar-Ishnava <84984682+Rameshwar-Ishnava@users.noreply.github.com>
2021-05-30 19:49:50 +0530

    Add files via upload

---
+++ Day 4/template1/partials/menu.php
@@ -0,0 +1,35 @@
+<section class="navbar custom-navbar navbar-fixed-top" role="navigation">
+          <div class="container">
+
+               <div class="navbar-header">
+                    <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                    </button>
+
+                    <!-- lOGO TEXT HERE -->
+                    <a href="" class="navbar-brand">RJ Technolab</a>
+               </div>
+
+               <!-- MENU LINKS -->
+               <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-nav-first">
+                         <li class=""><a href="index.php">Home</a></li>
+                         <li><a href="job-listing.php">Jobs</a></li>
+                         <li><a href="blog-posts.php">Blog</a></li>
+                         <li class="dropdown">
+                              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">More <span class="caret"></span></a>
+                              
+                              <ul class="dropdown-menu">
+                                   <li><a href="team.php">Team</a></li>
+                                   <li><a href="testimonials.php">Testimonials</a></li>
+                                   <li><a href="terms.php">Terms</a></li>
+                              </ul>
+                         </li>
+                         <li><a href="contact.php">Contact Us</a></li>
+                    </ul>
+               </div>
+
+          </div>
+     </section>
