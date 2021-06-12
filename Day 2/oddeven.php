599acb88338f191ad8f7b02098705b294aabd252
Rameshwar-Ishnava <84984682+Rameshwar-Ishnava@users.noreply.github.com>
2021-06-12 09:21:31 +0530

    Create oddeven.php

---
+++ Day 2/oddeven.php
@@ -0,0 +1,21 @@
+  
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Task-2 : Odd/Even
+	</title>
+</head>
+<body>
+	<h2>Odd or Even Program in PHP.</h2><br>	
+	<?php 
+	$input = 45;
+	if($input % 2 == 0){
+	echo "$input is Even";
+}
+	else{
+	echo("$input is Odd");
+}
+ ?>
+
+</body>
