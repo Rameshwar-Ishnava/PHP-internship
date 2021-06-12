4eddfa583670e41f9e922c5ce0764507966ca9d6
Rameshwar-Ishnava <84984682+Rameshwar-Ishnava@users.noreply.github.com>
2021-06-12 11:02:23 +0530

    Create forloop.php

---
+++ Day 3/forloop.php
@@ -0,0 +1,15 @@
+<?php
+echo "<table border='1'>";
+for($i=0;$i<10;$i++){
+	echo "<tr>";
+	if($i%2==0){
+		echo"<td>$i</td>";
+	}else{
+		echo"<td>$i</td>";
+	}
+	echo "</tr>";
+}
+echo "</table>";
+	
+
+?>
