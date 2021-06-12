a45d1552a48245366d49d7532fabe389634cb084
Rameshwar-Ishnava <84984682+Rameshwar-Ishnava@users.noreply.github.com>
2021-06-12 11:26:09 +0530

    Create mark.php

---
+++ Day 3/mark.php
@@ -0,0 +1,71 @@
+<html>
+<head>
+    <title>
+        mark
+    </title>
+</head>
+    <body>
+        <form action="result.php" method="post">
+            <center>
+                <table border=1 cellpadding=1px>
+                    <h2>Enter Marks and get result</h2>
+                    <tr>
+                        <td>
+                            Student Name
+                        </td>
+                        <td>
+                            <input type=text name="t1" required >
+                        </td>
+                    </tr>
+				
+                    <tr>
+                        <td>
+                            Marks for AJAVA
+                        </td>
+                        <td>
+                            <input type=text name="t2" required>
+                        </td>
+                    </tr>
+					
+                    <tr>
+                        <td>
+                            Marks for CNS
+                        </td>
+                        <td>
+                            <input type=text name="t3" required>
+                        </td>
+                    </tr>
+                    
+                    <tr>
+                        <td>
+                            Marks for PHP
+                        </td>
+                        <td>
+                            <input type=text name="t4" required>
+                        </td>
+                    </tr>
+                    
+                    <tr>
+                        <td>
+                            Marks for DWPD
+                        </td>
+                        <td>
+                            <input type=text name="t5" required>
+                        </td>
+                    </tr>
+                   
+                    <tr>
+                        <td>
+                            Marks for Android
+                        </td>
+                        <td>
+                            <input type=text name="t6" required>
+                        </td>
+                    </tr>
+                </table>
+                <br>
+                <input type=submit name="s" value="Result">
+                </center>
+        </form>
+    </body>
+</html>
