e4cebbe1e00e59fbdf36cb49ec5342b1ca8665d4
Rameshwar-Ishnava <84984682+Rameshwar-Ishnava@users.noreply.github.com>
2021-06-12 09:43:56 +0530

    Create switchcase.php

---
+++ Day 2/switchcase.php
@@ -0,0 +1,24 @@
+<?php      
+    $ch = "B.Tech";  
+    switch ($ch)  
+    {     
+         case "DIPLOMA":   
+            echo "DIPLOMA is 3 years course";  
+            break;  
+        case "BCA":   
+            echo "BCA is 3 years course";  
+            break;  
+        case "Bsc":   
+            echo "Bsc is 3 years course";  
+            break;  
+        case "B.Tech":   
+            echo "B.Tech is 4 years course";  
+            break;  
+        case "B.Arch":   
+            echo "B.Arch is 5 years course";  
+            break;  
+        default:   
+            echo "Wrong Choice";  
+            break;  
+    }  
+?>    
